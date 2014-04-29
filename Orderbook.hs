fac683a7ba98dcf4207d207c2e1b3ba8549ff8c4
James Byatt <grumpyjames@gmail.com>
2014-04-29 20:38:03 +0100

    failing test

--- Orderbook.hs
+++ Orderbook.hs
@@ -3,8 +3,11 @@ import Control.Monad
 import Test.QuickCheck
 import Test.Hspec
 
+type Events = [PlaceResult]
+
 data PlaceResult = Accepted                   
                  | Rejected String
+                 | Fill Price Quantity
                  deriving (Show, Eq)
               
 type Price = Int                     
@@ -19,17 +22,28 @@ type Orderbook = [Order]
 emptyOrderbook :: Orderbook
 emptyOrderbook = []
 
-placeOrder :: Order -> Orderbook -> (Orderbook, PlaceResult)
-placeOrder o ob = ([o], Accepted)
+placeOrder :: Order -> Orderbook -> (Orderbook, Events)
+placeOrder o ob = ([o], [Accepted])
+
+(>!>) :: (Orderbook, Events) -> (Orderbook -> (Orderbook, Events)) -> (Orderbook, Events)
+(>!>) tuple f = f $ fst tuple
 
 instance Arbitrary Order where
   arbitrary = liftM2 Order arbitrary arbitrary
 
+opposite :: Order -> Order
+opposite (Order p q) = Order p (-q)
+
+fullFillOf :: Order -> PlaceResult
+fullFillOf (Order p q) = Fill p q
+
 main = hspec $ do
   describe "Orderbooks" $ do
     it "always accepts an order if it is empty" $ property $ \o ->
-      (snd $ placeOrder o emptyOrderbook) == Accepted
+      (snd $ placeOrder o emptyOrderbook) == [Accepted]
     it "consists of the only order in them after a single place" $ property $ \o ->
       (fst $ placeOrder o emptyOrderbook) == [o]
+    it "fills both sides of equal and opposite orders when placed consecutively" $ property $ \o ->
+      placeOrder o emptyOrderbook >!> (placeOrder $ opposite o) == ([], [fullFillOf o, fullFillOf $ opposite o])
       
       
