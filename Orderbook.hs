68c981a2ee144f4d26494dbd0ee7a70dd2a06eca
James Byatt <grumpyjames@gmail.com>
2014-04-29 21:11:51 +0100

    test still failing

--- Orderbook.hs
+++ Orderbook.hs
@@ -26,8 +26,10 @@ validOrder (Order p q) = and [q /= 0, p /= 0]
 
 placeOrder :: Order -> Orderbook -> (Orderbook, Events)
 placeOrder o ob 
-  | validOrder o = ([o], [Accepted])
-  | otherwise = ([], [Rejected])
+  | validOrder o = walkBook o ob
+  | otherwise = ([], [Rejected])                               
+  where walkBook o [] = ([o], [Accepted])
+        walkBook (Order p1 q1) [(Order p2 q2)] = ([], [Fill p1 q1, Fill p2 q2])        
 
 (>!>) :: (Orderbook, Events) -> (Orderbook -> (Orderbook, Events)) -> (Orderbook, Events)
 (>!>) tuple f = f $ fst tuple
