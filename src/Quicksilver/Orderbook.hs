fbf4c3b0510e9a95e751991463052a976688ab22
James Byatt <grumpyjames@gmail.com>
2014-05-13 18:04:16 +0100

    Make match function a bit more realistic, make the matching cycle a fold, make a test pass

--- src/Quicksilver/Orderbook.hs
+++ src/Quicksilver/Orderbook.hs
@@ -24,21 +24,14 @@ type Orderbook = ([Order],[Order])
 data Side = Bid 
           | Ask
           deriving (Show, Eq)
+                   
+type Reconstitute = [Order] -> [Order] -> Orderbook         
+type Match = Order -> Order -> MatchResult
 
-data MatchResult = MatchResult (Maybe Order) (Maybe Order) Events 
+data MatchResult = MatchSuccess (Price, Quantity) (Maybe Order) (Maybe Order)
+                 | MatchFail Order Order
 
-class Matchable a where
-  match :: a -> a -> MatchResult
   
-instance Matchable Order where
-  match = matchOrders 
-  
-matchOrders :: Order -> Order -> MatchResult  
-matchOrders o1@(Order p1 q1) o2@(Order p2 q2)
-  | (signum q1) == (signum q2) = MatchResult (Just o1) (Just o2) []
-  | (abs q1) > (abs q2) = MatchResult (Just (Order p1 (q1+q2)))  Nothing [Fill p1 q2, Fill p1 (-q2)]
-  | otherwise = MatchResult Nothing (Just (Order p2 (q1+q2))) [Fill p1 (-q1), Fill p1 q1]
-
 side :: Order -> Side
 side (Order _ a)
   | a > 0 = Bid
@@ -51,21 +44,29 @@ opposite :: Side -> Side
 opposite Bid = Ask
 opposite Ask = Bid
 
-sideToScan :: Side -> Orderbook -> [Order]
-sideToScan Bid (_, a) = a
-sideToScan Ask (a, _) = a
+matchSide :: Side -> Orderbook -> [Order]
+matchSide Bid (_, a) = a
+matchSide Ask (a, _) = a
 
-sideToAcc :: Side -> Orderbook -> [Order]
-sideToAcc = sideToScan . opposite
+passiveSide :: Side -> Orderbook -> [Order]
+passiveSide = matchSide . opposite
 
 matches :: Side -> Match
-matches Bid = (>=)
-matches Ask = (<=)
+matches Bid = m (>=)
+matches Ask = m (<=)
+
+m :: (Int -> Int -> Bool) -> Match
+m gt o1@(Order p1 q1) o2@(Order p2 q2)
+  | p1 `gt` p2 = MatchSuccess (p2, fillQty) (remaining o1 fillQty) (remaining o2 (-fillQty))
+  | otherwise = MatchFail o1 o2
+  where fillQty = (signum q1) * min (abs q1) (abs q2)                  
+          
+remaining :: Order -> Quantity -> (Maybe Order)
+remaining (Order p q) fillQty
+  | q - fillQty /= 0 = Just $ Order p (q - fillQty)
+  | otherwise = Nothing
 
 validOrder (Order p q) = and [q /= 0, p > 0]
-
-type Reconstitute = [Order] -> [Order] -> Orderbook         
-type Match = Price -> Price -> Bool
 
 rebuild :: Side -> Reconstitute
 rebuild Bid a b = (b, a)
@@ -73,19 +74,35 @@ rebuild Ask a b = (a, b)
 
 placeOrder :: Order -> Orderbook -> (Orderbook, Events)
 placeOrder o ob 
-  | validOrder o = walkBook scanSide accSide o match reassemble
+  | validOrder o = walkBook match passive o matchFn reassemble
   | otherwise = (ob, [Rejected])                               
-  where scanSide = sideToScan orderSide ob
-        accSide = sideToAcc orderSide ob
+  where match = matchSide orderSide ob
+        passive = passiveSide orderSide ob
         reassemble = rebuild orderSide
-        match = matches orderSide
+        matchFn = matches orderSide
         orderSide = side o        
+        
+type FoldCtx = ([(Price,Quantity)], Maybe Order, [Order])
+
+foldStep :: Match -> Order -> FoldCtx -> FoldCtx
+foldStep m passiveOrder (fills, Nothing, book) = (fills, Nothing, passiveOrder:book)
+foldStep m passiveOrder (fills, Just aggressiveOrder, book) = merge (m aggressiveOrder passiveOrder) fills book
+  where merge :: MatchResult -> [(Price,Quantity)] -> [Order] -> FoldCtx
+        merge (MatchFail aggressiveOrder passiveOrder) fills book = (fills, Just aggressiveOrder, passiveOrder:book)
+        merge (MatchSuccess f@(p,quantity) Nothing Nothing) fills book = (f:fills, Nothing, book)
+        merge (MatchSuccess f@(p,quantity) r@(Just _) Nothing) fills book = (f:fills, r, book)
+        merge (MatchSuccess f@(p,quantity) Nothing (Just o)) fills book = (f:fills, Nothing, o:book)
+        merge (MatchSuccess f@(p,quantity) _ _) _ _ = undefined -- FIXME: restrict MatchResult to prevent this
         
 walkBook :: [Order] -> [Order] -> Order -> Match -> Reconstitute -> (Orderbook, Events)
 walkBook [] accSide o _ r = (r [] (insertBag o accSide), [Accepted])
-walkBook ((Order p1 q1):[]) accSide (Order p2 q2) m r 
-  | m p2 p1 = (remainder r p1 p2 q1 q2, [Fill p1 (-q1), Fill p1 q1])
-  | otherwise = (r [Order p1 q1] [Order p2 q2], [Accepted])
+walkBook scanSide accSide o m r = (r (reverse reverseBook) (insert leftover accSide), Accepted : (genFills fills))
+  where (fills, leftover, reverseBook) = foldr (foldStep m) ([], Just o, []) scanSide
+        insert Nothing accside = accside
+        insert (Just o) accside = insertBag o accside
+        genFills :: [(Price,Quantity)] -> Events
+        genFills ((p,q):xs) = (Fill p q):(Fill p (-q)):genFills(xs)
+        genFills [] = []
 
 -- avoid bringing in a lib and just implement this for the time being.
 insertBag :: (Ord a) => a -> [a] -> [a]
--- test/Quicksilver/OrderbookSpec.hs
+++ test/Quicksilver/OrderbookSpec.hs
@@ -1,4 +1,4 @@
-module Quicksilver.OrderbookSpec (runTests) where
+module Quicksilver.OrderbookSpec (runTests,placeOrders) where
 
 import Quicksilver.Orderbook
 
@@ -47,16 +47,16 @@ runTests = hspec $ do
     it "consists of the only order in them after a single place" $ property $ \o@(Order p q) ->
       and [p > 0, q /= 0] ==> (fst $ placeOrder o emptyOrderbook) == pickSide q [o]
     it "fills both sides of equal and opposite orders when placed consecutively" $ property $ \o@(Order p q)  ->
-      and[p > 0, q /= 0] ==> placeOrder o emptyOrderbook >!> (placeOrder $ opposite o) == (([],[]), [fullFillOf $ opposite o, fullFillOf o])
+      and[p > 0, q /= 0] ==> placeOrder o emptyOrderbook >!> (placeOrder $ opposite o) == (([],[]), [Accepted, fullFillOf $ opposite o, fullFillOf o])
     it "accepts the same order twice, leaving it on the book" $ property $ \o@(Order _ q) ->
       (validOrder o) ==> placeOrder o emptyOrderbook >!> placeOrder o == (pickSide q [o,o], [Accepted])
     it "places opposite orders that don't match on opposite sides of the book" $ property $ \(Order p q) ->
       and[p > 0, q > 0] ==> placeOrder (Order p q) emptyOrderbook >!> placeOrder (Order (p+1) (-q)) == (([Order p q],[Order (p+1) (-q)]), [Accepted])
     it "matches a buy order with a sell order with a lower price, the aggressing order taking the best price" $ property $ \(Order p q) ->
-      and[p > 1, q > 0] ==> placeOrder (Order p q) emptyOrderbook >!> placeOrder (Order (p-1) (-q)) == (([],[]), [Fill p (-q), Fill p q])
+      and[p > 1, q > 0] ==> placeOrder (Order p q) emptyOrderbook >!> placeOrder (Order (p-1) (-q)) == (([],[]), [Accepted, Fill p (-q), Fill p q])
     it "matches only half of a double reversed order, leaving the remainder on the book" $ property $ \(Order p q) ->
-      and[p > 0, q /= 0] ==> placeOrder (Order p q) emptyOrderbook >!> placeOrder (Order p ((-2)*q)) == (pickSide (-q) [Order p (-q)], [Fill p (-q), Fill p q])
+      and[p > 0, q /= 0] ==> placeOrder (Order p q) emptyOrderbook >!> placeOrder (Order p ((-2)*q)) == (pickSide (-q) [Order p (-q)], [Accepted, Fill p (-q), Fill p q])
     it "has the same representation regardless of the entry order of two orders with different prices" $ property $ \(o1@(Order p _),o2@(Order r _)) ->
       p /= r ==> placeOrders [o1, o2] == placeOrders [o2, o1]
-
-
+    it "can match two orders on the opposite side" $ property $ \q ->
+      q /= 0 ==> placeOrder (Order 1 ((-3)*q)) (placeOrders [Order 1 q, Order 1 (2*q)]) == (([],[]), [Accepted, Fill 1 ((-2)*q), Fill 1 (2*q), Fill 1 (-q), Fill 1 q])
